57e60b72722bc2ec99f31df434d50dc5e68c5706
Andrey Volkov <avolkov@mirantis.com>
2016-11-25 22:28:04 +0300

    Clean signal handler for subprocess

--- ttr/tests/test_integration.py
+++ ttr/tests/test_integration.py
@@ -1,3 +1,5 @@
+import os
+import signal
 import unittest
 from subprocess import Popen, PIPE
 from socket import create_connection
@@ -6,12 +8,29 @@ from ttr import server
 
 
 class TtrTestCase(unittest.TestCase):
-    def test_run_single_test(self):
-        p = Popen(['python', '/home/amadev/files/prog/ttr/bin/ttr'])
+
+    def _start(self):
+        self.p = Popen(['python', '/home/amadev/files/prog/ttr/bin/ttr'])
         time.sleep(0.2)
+        return self.p
+
+    def _stop(self):
+        self.p.terminate()
+        time.sleep(0.2)
+
+    def test_run_single_test(self):
+        p = self._start()
         conn = create_connection(server.ADDRESS)
         conn.send('ttr.tests.test_server.ServerTestCase.test_listen---')
         test_result = conn.recv(1024)
         self.assertEqual('ok', test_result)
         conn.close()
-        p.terminate()
+        self._stop()
+
+    def test_restart_test_runner(self):
+        p = self._start()
+        os.kill(p.pid, signal.SIGHUP)
+        time.sleep(0.2)
+        os.kill(p.pid, signal.SIGHUP)
+        time.sleep(0.2)
+        self._stop()
