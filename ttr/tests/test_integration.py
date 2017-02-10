89987ddfaca85871f3ffe6fe6b650d437bded60d
Andrey Volkov <avolkov@mirantis.com>
2017-02-10 23:32:18 +0300

    Add integration test cases

--- ttr/tests/test_integration.py
+++ ttr/tests/test_integration.py
@@ -9,17 +9,19 @@ from ttr import server
 ROOT_PATH = __file__
 for i in range(3):
     ROOT_PATH = os.path.dirname(ROOT_PATH)
+WAIT_TIME = 0.5
+
 
 class TtrTestCase(unittest.TestCase):
 
     def setUp(self):
         self.p = Popen(['python', ROOT_PATH + '/bin/ttr-start'])
-        time.sleep(0.3)
+        time.sleep(WAIT_TIME)
         return self.p
 
     def tearDown(self):
         self.p.terminate()
-        time.sleep(0.3)
+        time.sleep(WAIT_TIME)
 
     def test_run_single_test(self):
         conn = create_connection(server.ADDRESS)
@@ -31,9 +33,9 @@ class TtrTestCase(unittest.TestCase):
 
     def test_restart_test_runner(self):
         os.kill(self.p.pid, signal.SIGHUP)
-        time.sleep(0.2)
-        os.kill(self.p.pid, signal.SIGHUP)
-        time.sleep(0.2)
+        time.sleep(WAIT_TIME)
+        conn = create_connection(server.ADDRESS)
+        self.assert_list_tests_work(conn)
 
     def test_be_resilent_after_not_found_test(self):
         conn = create_connection(server.ADDRESS)
@@ -46,16 +48,29 @@ class TtrTestCase(unittest.TestCase):
         test_result = conn.recv(1024)
         self.assertIn('Ran 0 tests in', test_result)
         conn.send(
-            'run_tests|ttr.tests.test_server.ServerTestCase.test_listen---')
+            'run_tests|'
+            'ttr.tests.test_server.ServerTestCase.test_read_tests---')
         test_result = conn.recv(1024)
         self.assertIn('Ran 1 test in', test_result)
         conn.close()
 
-    def test_filter_tests(self):
+    def test_run_multiple_tests_at_once(self):
         conn = create_connection(server.ADDRESS)
+        conn.send(
+            'run_tests|'
+            'ttr.tests.test_server.ServerTestCase.test_read_tests'
+            'ttr.tests.test_server.ServerTestCase.test_listen---')
+        test_result = conn.recv(1024)
+        self.assertIn('Ran 2 tests in', test_result)
+
+    def assert_list_tests_work(self, conn):
         conn.send('list_tests|ServerTestCase.test_read_tests---')
         test_result = conn.recv(1024)
         self.assertIn(
             'ttr.tests.test_server.ServerTestCase.test_read_tests',
             test_result)
         self.assertEqual(1, len(test_result.split('\n')))
+
+    def test_filter_tests(self):
+        conn = create_connection(server.ADDRESS)
+        self.assert_list_tests_work(conn)
